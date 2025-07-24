2871cf75755303f8f97fa026987a29d005a64804
ELAKIYA2000 <65751853+ELAKIYA2000@users.noreply.github.com>
2025-07-24 13:45:40 +0530

    Add files via upload

---
+++ SmartporterApplication.java
@@ -0,0 +1,13 @@
+package com.reboot.smartporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartporterApplication.class, args);
+	}
+
+}
